e8a8a7bf96afbbe569cbb9e2cc5234b466d00592
Vincent <vncntduong@gmail.com>
2019-02-13 19:43:06 -0800

    added bonuses editor for affix

--- loot-td-editor/AffixBase.cs
+++ loot-td-editor/AffixBase.cs
@@ -3,74 +3,107 @@ using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
-namespace loot_td {
-
-public class AffixBase
+namespace loot_td
 {
-    [JsonProperty]
-    public int Id { get; set; }
-    [JsonProperty]
-    public string Name { get; set; }
-    [JsonConverter(typeof(StringEnumConverter))] [JsonProperty]
-    public AffixType AffixType { get; set; }
-    [JsonProperty]
-    public int Tier { get; set; }
-    [JsonProperty]
-    public int SpawnLevel { get; set; }
-    [JsonProperty]
-    public List<AffixBonus> AffixBonuses { get; }
-    [JsonProperty]
-    public Dictionary<GroupType, int> SpawnWeight { get; }
-    [JsonProperty]
-    public List<GroupType> GroupTypes { get; }
 
-    public AffixBase()
+    public class AffixBase
     {
-        AffixBonuses = new List<AffixBonus>();
-        SpawnWeight = new Dictionary<GroupType, int>();
-        GroupTypes = new List<GroupType>();
-    }
+        [JsonProperty]
+        public int Id { get; set; }
+        [JsonProperty]
+        public string Name { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty]
+        public AffixType AffixType { get; set; }
+        [JsonProperty]
+        public int Tier { get; set; }
+        [JsonProperty]
+        public int SpawnLevel { get; set; }
+        [JsonProperty]
+        public List<AffixBonus> AffixBonuses { get; }
+        [JsonProperty]
+        public Dictionary<GroupType, int> SpawnWeight { get; }
+        [JsonProperty]
+        public List<GroupType> GroupTypes { get; }
 
-    public string ListString { get { return GetBonusTypesString(); } }
-
-    public string GetBonusTypesString()
-    {
-        string x = "(";
-        foreach (var a in AffixBonuses)
+        public AffixBase()
         {
-            x += a.bonusType.ToString();
-            x += ", ";
+            AffixBonuses = new List<AffixBonus>();
+            SpawnWeight = new Dictionary<GroupType, int>();
+            GroupTypes = new List<GroupType>();
         }
-        x.TrimEnd(',');
-        x += ")";
-        return x;
+
+        public AffixBase(AffixBase a)
+        {
+            Id = a.Id;
+            Name = a.Name;
+            AffixType = a.AffixType;
+            Tier = a.Tier;
+            SpawnLevel = a.SpawnLevel;
+
+            AffixBonuses = new List<AffixBonus>();
+            foreach (var b in a.AffixBonuses)
+            {
+                AffixBonus bonus = new AffixBonus
+                {
+                    BonusType = b.BonusType,
+                    ModifyType = b.ModifyType,
+                    MinValue = b.MinValue,
+                    MaxValue = b.MaxValue
+                };
+                AffixBonuses.Add(bonus);
+            }
+
+            SpawnWeight = new Dictionary<GroupType, int>(a.SpawnWeight);
+            GroupTypes = new List<GroupType>(a.GroupTypes);
+        }
+
+        public string ListString { get { return GetBonusTypesString(); } }
+
+        public string GetBonusTypesString()
+        {
+            string x = "(";
+            foreach (var a in AffixBonuses)
+            {
+                x += a.BonusType.ToString();
+                x += ", ";
+            }
+            x.TrimEnd(',');
+            x += ")";
+            return x;
+        }
+
+        public string GetBonusCountString { get { return AffixBonuses.Count.ToString(); } }
     }
-}
 
-public class AffixBonus
-{
-    [JsonConverter(typeof(StringEnumConverter))] [JsonProperty]
-    public BonusType bonusType;
-    [JsonConverter(typeof(StringEnumConverter))] [JsonProperty]
-    public ModifyType modifyType;
-    [JsonProperty]
-    public int minValue;
-    [JsonProperty]
-    public int maxValue;
-}
+    public class AffixBonus
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty]
+        public BonusType BonusType { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
+        [JsonProperty]
+        public ModifyType ModifyType { get; set; }
+        [JsonProperty]
+        public int MinValue { get; set; }
+        [JsonProperty]
+        public int MaxValue { get; set; }
+    }
 
-public enum AffixType
-{
-    PREFIX,
-    SUFFIX,
-    ENCHANTMENT
-}
+    public enum AffixType
+    {
+        PREFIX,
+        SUFFIX,
+        ENCHANTMENT,
+        INNATE
+    }
 
-public enum ModifyType
-{
-    ADDITIVE,
-    MULTIPLY,
-    SET
-}
+    public enum ModifyType
+    {
+        ADDITIVE,       //all sources add together before modifying
+        MULTIPLY,       //all sources multiply together before modifying
+        SET,            //sets value to modifier value, ignores all other increases
+        FLAT_ADDITION   //adds to base before any other calculation
+    }
 
 }
--- loot-td-editor/AffixEditor.xaml.cs
+++ loot-td-editor/AffixEditor.xaml.cs
@@ -1,19 +1,10 @@
-﻿using System;
+﻿using loot_td;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Collections.ObjectModel;
-using loot_td;
+using System.Diagnostics;
 
 namespace loot_td_editor
 {
@@ -24,34 +15,45 @@ namespace loot_td_editor
 
     public partial class AffixEditor : UserControl
     {
-        public ObservableCollection<AffixBase> Affixes = new ObservableCollection<AffixBase>();
+        public List<AffixBase> Affixes = new List<AffixBase>();
 
         private int currentID = 0;
 
-        public string AffixType
+
+
+        public string AffixProp
         {
-            get { return (string)GetValue(AffixTypeProperty); }
-            set { SetValue(AffixTypeProperty, value); }
+            get { return (string)GetValue(AffixPropProperty); }
+            set { SetValue(AffixPropProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for AffixType.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty AffixTypeProperty =
-            DependencyProperty.Register("AffixType", typeof(string), typeof(AffixEditor), new PropertyMetadata("PREFIX"));
+        // Using a DependencyProperty as the backing store for AffixProp.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AffixPropProperty =
+            DependencyProperty.Register("AffixProp", typeof(string), typeof(AffixEditor));
+
+
 
         public AffixType affixContext;
+        private bool isContextSet = false;
+
 
         public AffixEditor()
         {
             InitializeComponent();
             AffixesList.ItemsSource = Affixes;
-            Enum.TryParse(AffixType, out AffixType aff);
-            affixContext = aff;
-            currentID = Affixes.Count;
+            if (Affixes.Count >= 1)
+                currentID = Affixes[Affixes.Count - 1].Id+1;
+            else
+                currentID = 0;
         }
 
         private void AddButtonClick(object sender, RoutedEventArgs e)
         {
-
+            if (!isContextSet)
+            {
+                Enum.TryParse(AffixProp, out AffixType aff);
+                affixContext = aff;
+            }
             AffixBase temp = new AffixBase
             {
                 Id = currentID,
@@ -60,13 +62,65 @@ namespace loot_td_editor
             };
             Affixes.Add(temp);
             AffixesList.Items.Refresh();
-            currentID = Affixes.Count;
+            currentID++;
+        }
+
+        private void CopyButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (AffixesList.SelectedItem == null)
+                return;
+            AffixBase temp = new AffixBase((AffixBase)AffixesList.SelectedItem)
+            {
+                Id = currentID
+            };
+            Affixes.Add(temp);
+            AffixesList.Items.Refresh();
+            currentID++;
         }
 
         private void RemoveButtonClick(object sender, RoutedEventArgs e)
         {
+            if (AffixesList.SelectedItem == null)
+                return;
             Affixes.Remove((AffixBase)AffixesList.SelectedItem);
             AffixesList.Items.Refresh();
+        }
+
+        private void AddBonusButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (AffixesList.SelectedItem == null)
+                return;
+            AffixBonus b = new AffixBonus();
+            AffixBase temp = (AffixBase)AffixesList.SelectedItem;
+            temp.AffixBonuses.Add(b);
+            BonusGrid.Items.Refresh();
+            CountText.Text = "Count: " + temp.GetBonusCountString;
+        }
+
+        private void RemoveBonusButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (AffixesList.SelectedItem == null)
+                return;
+            AffixBase temp = (AffixBase)AffixesList.SelectedItem;
+            temp.AffixBonuses.Remove((AffixBonus)BonusGrid.SelectedItem);
+            BonusGrid.Items.Refresh();
+            CountText.Text = "Count: " + temp.GetBonusCountString;
+        }
+
+        private void BonusGrid_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            AffixesList.Items.Refresh();
+        }
+
+        private void BonusGrid_CurrentCellChanged(object sender, EventArgs e)
+        {
+            AffixesList.Items.Refresh();
+            
+        }
+
+        private void Binding_SourceUpdated(object sender, System.Windows.Data.DataTransferEventArgs e)
+        {
+
         }
     }
 }
